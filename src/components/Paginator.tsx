5b7d6d628e43dfc492294fb5a3ac3575854d1b04
Poocinka15 <poociek15@gmail.com>
2023-01-15 12:25:36 +0100

    Add previous and next arrows

--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -11,39 +11,37 @@ function Paginator({onClick, current, numberOfPages}: Props) {
     const [isFirst, setIsFirst] = useState(true);
     const [isLast, setIsLast] = useState(false);
 
-    //// ogarnąc tutaj z tymi strzałkami xd
     const goToPrevious = () => {
         if (current > 1) {
             onClick((current) => (current - 1));
-            setIsLast(false);
-        } else if (current === 1) {
-            setIsFirst(true);
         }
     }
     const goToNext = () => {
         if (current < numberOfPages) {
             onClick((current) => (current + 1));
-            setIsFirst(false);
-        } else if (current === numberOfPages) {
-            setIsLast(true);
         }
     }
-
+    // odpowiednie wyświetlanie strzałek
     useEffect(() => {
+        if (current === 1) {
+            setIsFirst(true);
+        } else if (current === numberOfPages)
+            setIsLast(true);
+        else {
+            setIsLast(false);
+            setIsFirst(false);
+        }
+    }, [current]);
 
-    }, [current] )
     return (
         <div>
             {!isFirst ? <div onClick={goToPrevious}>
                 previous
             </div> : null}
-
             {!isLast ? <div onClick={goToNext}>
                 next
             </div> : null}
-
         </div>
-
     );
 }
 
