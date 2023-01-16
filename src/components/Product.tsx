c940fb34c114fcc539a25f4b00502a8748fefa0c
Poocinka15 <poociek15@gmail.com>
2023-01-16 17:51:44 +0100

    Add some styling

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,23 +1,25 @@
 import React, {useContext} from 'react';
 import {IProduct} from "./ProductsList";
 import {SelectedIdContext} from "../App";
+import {ListItem, ListItemText} from "@mui/material";
 
 function Product(product: IProduct) {
     const context = useContext(SelectedIdContext);
 
     const {id, name, year, color, pantone_value: pantoneValue} = product;
+    const handleClick = () => {
+        if (!context?.isShowingModal) {
+            context?.setSelectedId(id);
+            context?.setIsShowingModal(() => true);
+        }
+    }
     return (
-        <div style={{backgroundColor: color}} onClick={() => {
-            if (!context?.isShowingModal) {
-                context?.setSelectedId(id);
-                context?.setIsShowingModal(() => true);
-            }
-        }}>
-            {name}
-            {year}
-            {pantoneValue}
 
-        </div>
+        <ListItem sx={{backgroundColor: color}} onClick={handleClick}>
+            <ListItemText primary={name}
+            secondary={`${year} ${pantoneValue}`}/>
+        </ListItem>
     );
 }
+
 export default Product;
