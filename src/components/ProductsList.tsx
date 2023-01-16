98e8996a88168862b0fe13c63222d10b39b7203e
Poocinka15 <poociek15@gmail.com>
2023-01-16 17:50:52 +0100

    Add react-router

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,7 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {getProductById, getProductsPerPage} from "../services/ProductsService";
 import Product from "./Product";
 import Paginator from "./Paginator";
+import {useSearchParams} from "react-router-dom";
+
+import {useNavigate, createSearchParams} from 'react-router-dom';
+import {ErrorMessageContext, SelectedIdContext} from "../App";
+import {Box, CircularProgress, List} from "@mui/material";
 
 export interface IProduct {
     id: number;
@@ -19,10 +24,32 @@ function ProductsList({query}: IQuery) {
 
     const [products, setProducts] = useState(Array<IProduct>)
     const [isLoading, setIsLoading] = useState(true);
-
     const [numberOfPages, setNumberOfPages] = useState(0);
-
     const [currentPage, setCurrentPage] = useState(1);
+
+    const selectedIdContext = useContext(SelectedIdContext);
+    const errorMessageContext = useContext(ErrorMessageContext);
+
+    const navigate = useNavigate();
+
+    const [searchParams] = useSearchParams();
+
+    useEffect(() => {
+
+        if (selectedIdContext !== null) {
+            const params = {
+                id: (selectedIdContext?.selectedId)?.toString(),
+                page: currentPage.toString()
+            };
+            const options = {
+                pathname: '',
+                search: `?${createSearchParams(params)}`,
+            };
+            navigate(options, {replace: true});
+
+        }
+    }, [currentPage, selectedIdContext?.selectedId]);
+
 
     const handleData = (result: { total_pages: number, data: [] }) => {
         setProducts(result.data);
@@ -35,32 +62,41 @@ function ProductsList({query}: IQuery) {
         setNumberOfPages(1);
     }
 
+    const handleError = () => {
+        // errorMessageContext?.setErrorMessage()
+    }
+
     useEffect(() => {
         if (query === "") {
-            getProductsPerPage(handleData, currentPage);
+            getProductsPerPage(currentPage, handleData);
         } else {
-            getProductById(parseInt(query), setFilteredData);
+            getProductById(parseInt(query), setFilteredData, handleError);
         }
 
     }, [currentPage, query]);
 
-
     return (
-        <div>
-            {isLoading ? "is loading" : null}
+        <>
+            <List sx={{minHeight: "350px"}}>
+                {isLoading ? <Box sx={{display: 'flex'}}>
+                    <CircularProgress/>
+                </Box> : null}
 
-            {products.map((p: IProduct) => {
-                return (
-                    <Product id={p.id}
-                             key={`product-${p.id}`}
-                             year={p.year}
-                             name={p.name}
-                             color={p.color}
-                             pantone_value={p.pantone_value}/>
-                )
-            })}
+                {products.map((p: IProduct) => {
+                    return (
+                        <Product id={p.id}
+                                 key={`product-${p.id}`}
+                                 year={p.year}
+                                 name={p.name}
+                                 color={p.color}
+                                 pantone_value={p.pantone_value}/>
+                    )
+                })}
+            </List>
             <Paginator onClick={setCurrentPage} current={currentPage} numberOfPages={numberOfPages}/>
-        </div>
+
+        </>
+
     );
 }
 
